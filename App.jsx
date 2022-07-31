5bcb26eddc4b7f5b418a9d91f76dc31873f6a2ef
Wadson <vavalson@hotmail.com>
2022-07-31 15:15:52 -0400

    feat(welcome screen): add welcome screen

--- App.jsx
+++ App.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import WelcomeScreen from './src/screens/Welcome.screen';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// })
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Some text</Text>
+    <View>
+      <WelcomeScreen />
       <StatusBar />
     </View>
   );
