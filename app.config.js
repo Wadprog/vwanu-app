569c5e4f832c6fdd7d6e56e988811629fd3b36e0
Wadson Vaval <vavalson@hotmail.com>
2025-02-18 09:56:03 -0500

    chore(scope): configure eas build

--- app.config.js
+++ app.config.js
@@ -6,5 +6,11 @@ export default {
         projectId: 'e550dc60-d34d-4c1d-8286-c670002c81d3',
       },
     },
+    runtimeVersion: {
+      policy: 'sdkVersion',
+    },
+    updates: {
+      url: 'https://u.expo.dev/e550dc60-d34d-4c1d-8286-c670002c81d3',
+    },
   },
 }
